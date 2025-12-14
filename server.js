cbbeca924bb8e2a72cbd739f86a43034034c6e86
Revathi123-git <revathiparisarla@gamil.com>
2025-12-15 00:22:05 +0530

    updated cores code

--- server.js
+++ server.js
@@ -18,18 +18,7 @@ const app = express();
   })
 ); 
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+
 
 app.use(express.json());
 
